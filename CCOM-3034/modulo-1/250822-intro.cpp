2542b1a8245fca05e5f7f9e78ef6a7d6087efb62
des <desteny282@gmil.com>
2025-08-22 23:20:30 -0400

    updating

---
+++ CCOM-3034/modulo-1/250822-intro.cpp
@@ -0,0 +1,13 @@
+// the way i forgot how to compile a file in c++
+
+// HOW TO COMPILE A FILE IN C++
+// g++ -o [NAME OF EXE] [CPP FILE PATH]
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hai" << endl;
+	return 0;
+}
